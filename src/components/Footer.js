4209cbb7ff2345979ba96517ad0208d54c435a0c
sarthak <thesarthakchauhan@gmail.com>
2024-10-21 14:55:40 +0530

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Footer = () => {
-  return (
+  return (<>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5790757708486073"
+        crossorigin="anonymous"></script>
+    <ins className="adsbygoogle"
+        style={{ display: 'block', textAlign: 'center' }}
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-5790757708486073"
+        data-ad-slot="1398638388"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto flex justify-between items-center">
         <p className="text-sm">
@@ -22,6 +34,7 @@ const Footer = () => {
         </p>
       </div>
     </footer>
+    </>
   );
 };
 
